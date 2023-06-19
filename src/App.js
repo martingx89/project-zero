a047fcfae12a3acb4c25fad8f8dd2162051b551c
Marcin Adamiak <marcin_89@outlook.com>
2023-06-19 19:15:54 +0200

    Add route for about and change colors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import NavBar from './components/views/NavBar/NavBar';
 import Home from './components/pages/Home/Home';
+import About from './components/pages/About/About';
 import NotFound from './components/pages/NotFound/NotFound';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/' element={<About />} />
             <Route path='/*' element={<NotFound />} />
           </Routes>
         </Container>
