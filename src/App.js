0aecc2f268d9026ea624b091a55f2c77c30736d8
Marcin Adamiak <marcin_89@outlook.com>
2023-06-19 18:57:12 +0200

    Fix pckages and add route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import NavBar from './components/views/NavBar/NavBar';
+import Home from './components/pages/Home/Home';
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <main>
+        <NavBar />
+        <Container>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default App;
