779e66459228ecdd15946148fc1f0eb7fd8b6b9e
Marcin Adamiak <marcin_89@outlook.com>
2023-06-19 21:56:43 +0200

    Get data images to Home

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,4 +1,9 @@
+import { useSelector } from 'react-redux';
+import { getAllImages } from '../../../redux/heroReducer';
+
 const Home = () => {
+  const images = useSelector(getAllImages);
+  console.log(images);
   return;
 };
 
