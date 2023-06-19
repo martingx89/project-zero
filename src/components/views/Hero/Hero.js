fe9c0515e14acf9188f6d54380b44058a147b6ad
Marcin Adamiak <marcin_89@outlook.com>
2023-06-19 22:30:18 +0200

    Clean code

--- src/components/views/Hero/Hero.js
+++ src/components/views/Hero/Hero.js
@@ -1,11 +1,6 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllImages } from '../../../redux/heroReducer';
 import { randomGenerator } from '../../../utils/randomGenerator';
 
 const Hero = () => {
-  // const images = useSelector(getAllImages);
-  // console.log(images);
-  // const dispatch = useDispatch();
   const number = randomGenerator(1, 3);
   return (
     <>
