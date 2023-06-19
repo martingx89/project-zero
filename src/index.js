e15f79381414d5ae3d218583a610b719aaea7fe9
Marcin Adamiak <marcin_89@outlook.com>
2023-06-19 18:26:42 +0200

    Prepare styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/normalize.scss';
+import './styles/global.scss';
 import { Container } from 'react-bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
