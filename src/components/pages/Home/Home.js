eb1d3b784ecf940755e949d020d67804218f5733
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-06-20 11:13:06 +0000

    Add accordion data from state

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -45,17 +45,13 @@ const Home = () => {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-      <Accordion defaultActiveKey='0' flush>
-        <Accordion.Item eventKey='0'>
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
+      <Accordion defaultActiveKey='1' flush>
+        {cards.map((card) => (
+          <Accordion.Item eventKey={card.id}>
+            <Accordion.Header>{card.title}</Accordion.Header>
+            <Accordion.Body>{card.descriptionShort}</Accordion.Body>
+          </Accordion.Item>
+        ))}
       </Accordion>
     </>
   );
