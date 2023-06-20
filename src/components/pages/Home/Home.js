bfd5fdab4021ad17e1daf294c0ecc744acfffb4d
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-06-20 09:00:19 +0000

    Try witch accordin

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,44 +1,68 @@
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Accordion } from 'react-bootstrap';
 
 const Home = () => {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className='d-block w-100'
-          src={`${process.env.PUBLIC_URL}/images/hero/hero_lesko_01.jpg`}
-          alt='First slide'
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className='d-block w-100'
-          src={`${process.env.PUBLIC_URL}/images/hero/hero_lesko_02.jpg`}
-          alt='Second slide'
-        />
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src={`${process.env.PUBLIC_URL}/images/hero/hero_lesko_01.jpg`}
+            alt='First slide'
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src={`${process.env.PUBLIC_URL}/images/hero/hero_lesko_02.jpg`}
+            alt='Second slide'
+          />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className='d-block w-100'
-          src={`${process.env.PUBLIC_URL}/images/hero/hero_lesko_03.jpg`}
-          alt='Third slide'
-        />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src={`${process.env.PUBLIC_URL}/images/hero/hero_lesko_03.jpg`}
+            alt='Third slide'
+          />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <Accordion defaultActiveKey='0' flush>
+        <Accordion.Item eventKey='0'>
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey='1'>
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </>
   );
 };
 
