ac8fca5890245d0686edd726086ed1caf01b0bc6
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-06-20 12:58:23 +0000

    Try add title

--- src/components/pages/Card/Card.js
+++ src/components/pages/Card/Card.js
@@ -1,10 +1,17 @@
 import { useSelector } from 'react-redux';
 import { getCardById } from '../../../redux/cardReducer';
+import PageTitle from '../../common/PageTitle/PageTitle';
 
 const Card = () => {
   const cardId = useSelector(getCardById);
   console.log(cardId);
-  return <></>;
+  return (
+    <>
+      {cardId.map((card) => (
+        <PageTitle>{card.title}</PageTitle>
+      ))}
+    </>
+  );
 };
 
 export default Card;
