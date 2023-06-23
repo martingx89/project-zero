f35ba472bceb13fef9bcd56d8c09497c4a81a188
Marcin Adamiak <marcin_89@outlook.com>
2023-06-23 20:18:29 +0200

    Style indicator

--- src/components/features/LevelIndicator.js
+++ src/components/features/LevelIndicator.js
@@ -3,7 +3,7 @@ import styles from './LevelIndicator.module.scss';
 const LevelIndicator = (props) => {
   return (
     <>
-      <div className={styles.green}>{props.children}</div>
+      <div className={styles.box}>{props.children}</div>
     </>
   );
 };
